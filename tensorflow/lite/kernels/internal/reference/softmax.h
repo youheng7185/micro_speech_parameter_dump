13248efabf7746594f407777958a2b406a19df00
youheng7185 <youheng7185@gmail.com>
2026-06-11 06:38:56 +0800

    dump softmax params

--- tensorflow/lite/kernels/internal/reference/softmax.h
+++ tensorflow/lite/kernels/internal/reference/softmax.h
@@ -68,9 +68,15 @@ template <typename InputT, typename OutputT>
 inline void Softmax(const SoftmaxParams& params,
                     const RuntimeShape& input_shape, const InputT* input_data,
                     const RuntimeShape& output_shape, OutputT* output_data) {
+
+  MicroPrintf("softmax in");
   const int32_t input_beta_multiplier = params.input_multiplier;
   const int32_t input_beta_left_shift = params.input_left_shift;
   const int diff_min = params.diff_min;
+
+  MicroPrintf("input beta multipier %d", input_beta_multiplier);
+  MicroPrintf("input_beta_left_shift %d", input_beta_left_shift);
+  MicroPrintf("diff_min %d", diff_min);
   // The representation chosen for the input to the exp() function is Q5.26.
   // We need to leave extra space since values that we skip might be as large as
   // -32 before multiplying by input_beta_multiplier, and therefore as large as
@@ -90,10 +96,13 @@ inline void Softmax(const SoftmaxParams& params,
   const int depth =
       MatchingDim(input_shape, trailing_dim, output_shape, trailing_dim);
 
+  MicroPrintf("outer_size: %d", outer_size);
+  MicroPrintf("depth: %d", depth);
   for (int i = 0; i < outer_size; ++i) {
     InputT max_in_row = std::numeric_limits<InputT>::min();
     for (int c = 0; c < depth; ++c) {
       max_in_row = std::max(max_in_row, input_data[i * depth + c]);
+      MicroPrintf("input data: %d", input_data[i * depth + c]);
     }
 
     FixedPointAccum sum_of_exps = FixedPointAccum::Zero();
@@ -104,18 +113,30 @@ inline void Softmax(const SoftmaxParams& params,
         const int32_t input_diff_rescaled =
             MultiplyByQuantizedMultiplierGreaterThanOne(
                 input_diff, input_beta_multiplier, input_beta_left_shift);
+        MicroPrintf("input diff: %d, input diff rescaled: %d", input_diff, input_diff_rescaled);
         const FixedPointScaledDiff scaled_diff_f8 =
             FixedPointScaledDiff::FromRaw(input_diff_rescaled);
+        MicroPrintf("scaled diff f8: 0x%x", scaled_diff_f8);
+
+        MicroPrintf("exp on neg ans: 0x%x", gemmlowp::Rescale<kAccumulationIntegerBits>(
+                                        exp_on_negative_values(scaled_diff_f8)));
+
         sum_of_exps = sum_of_exps + gemmlowp::Rescale<kAccumulationIntegerBits>(
                                         exp_on_negative_values(scaled_diff_f8));
       }
     }
 
+    MicroPrintf("sum of exp: 0x%x", sum_of_exps);
+
     int num_bits_over_unit;
     FixedPoint0 shifted_scale = FixedPoint0::FromRaw(GetReciprocal(
         sum_of_exps.raw(), kAccumulationIntegerBits, &num_bits_over_unit));
-
+    MicroPrintf("shifted_scale: 0x%x", shifted_scale);
+    
+    MicroPrintf("num_bits_over_unit: %d", num_bits_over_unit);
     const int exponent = num_bits_over_unit + 31 - (sizeof(OutputT) * 8);
+
+    MicroPrintf("exponent: %d", exponent);
     TFLITE_CHECK(0 <= exponent && exponent <= 31);
 
     for (int c = 0; c < depth; ++c) {
@@ -125,16 +146,24 @@ inline void Softmax(const SoftmaxParams& params,
         const int32_t input_diff_rescaled =
             MultiplyByQuantizedMultiplierGreaterThanOne(
                 input_diff, input_beta_multiplier, input_beta_left_shift);
+        MicroPrintf("input_diff_rescaled: %d", input_diff_rescaled);
+
         const FixedPointScaledDiff scaled_diff_f8 =
             FixedPointScaledDiff::FromRaw(input_diff_rescaled);
+        MicroPrintf("scaled_diff_f8: 0x%x", scaled_diff_f8);
 
         FixedPoint0 exp_in_0 = exp_on_negative_values(scaled_diff_f8);
+        MicroPrintf("exp_in_0: 0x%x", exp_in_0);
+
         int32_t unsat_output = gemmlowp::RoundingDivideByPOT(
             (shifted_scale * exp_in_0).raw(), exponent);
+        MicroPrintf("\nshifted_scale * exp_in_0: 0x%x", shifted_scale * exp_in_0);
+        MicroPrintf("unsat_output: %d", unsat_output);
 
         const int32_t shifted_output =
             unsat_output +
             static_cast<int32_t>(std::numeric_limits<OutputT>::min());
+        MicroPrintf("shifted_output: %d", shifted_output);
 
         output_data[i * depth + c] = static_cast<OutputT>(std::max(
             std::min(shifted_output,
@@ -143,6 +172,8 @@ inline void Softmax(const SoftmaxParams& params,
       } else {
         output_data[i * depth + c] = std::numeric_limits<OutputT>::min();
       }
+
+      MicroPrintf("output data: %d", output_data[i * depth + c]);
     }
   }
 }
